a010aad4bc28b98048ac5458d91b8a38a9c8eee7
Jennifer Appleby <33709515+jenapp@users.noreply.github.com>
2018-04-23 17:03:32 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,14 +6,15 @@ templates_path = ['_templates']
 
 project = u'Smile'
 #copyright = u'2018, Inomial Pty Ltd'
-copyright = '2018-{}, Inomial Pty Ltd & contributors'
+copyright = '2018, Inomial Pty Ltd & contributors'
 
 #html_logo = 'logo.png'
 #html_static_path = ['_static']
 
 #html_theme_path = ['_themes']
 
-html_theme = 'default'
+#html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_logo = 'logo.png'
